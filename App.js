fda72838d7dc06c5008ad87b623f21992067744c
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-11-03 23:32:38 +0000

    update

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src/Home';
+import About from './src/About';
+import Note from './src/Note';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Note' component={Note} />
+        <Stack.Screen name='About' component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
