e7852ac45c140d7a4cc2674dec0619f175d787f1
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-11-03 23:32:46 +0000

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Pressable,
+  ScrollView,
+} from 'react-native';
+import { useState, useRef } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
