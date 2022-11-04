a77adb0de9003e0647127bf28fa7ead5257f34c4
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-11-04 01:53:03 +0000

    Update App.js

--- App.js
+++ App.js
@@ -5,18 +5,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/Home';
 import About from './src/About';
 import Note from './src/Note';
+import { NativeBaseProvider } from 'native-base';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='Note' component={Note} />
-        <Stack.Screen name='About' component={About} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name='Note' component={Note} />
+          <Stack.Screen name='About' component={About} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
